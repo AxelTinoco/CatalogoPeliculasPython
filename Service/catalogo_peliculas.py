c3a42255a8784874cb2549fc6059f2cd7f161596
AxelTinoco <axeltm8@gmail.com>
2022-02-16 14:49:42 -0600

    Finish class catalogo

--- Service/catalogo_peliculas.py
+++ Service/catalogo_peliculas.py
@@ -1,3 +1,6 @@
+import os
+
+
 class CatalogoPeliculas:
     ruta_archivo = 'peliculas.txt'
 
@@ -5,3 +8,15 @@ class CatalogoPeliculas:
     def agregar_pelicula(cls,pelicula):
         with open(cls.ruta_archivo,'a', encoding='utf8') as archivo:
             archivo.write(f'{pelicula.nombre}')
+
+
+    @classmethod
+    def listar_peliula(cls):
+        with open(cls.ruta_archivo,'r,',encoding='utf8') as archivo:
+            print('Catalogo de peliculas'.center(50,'-'))
+            print(archivo.read())
+
+    @classmethod
+    def eliminar_pelicula(cls):
+        os.remove(cls.ruta_archivo)
+        print(f'Se elimino la pelicula : {cls.ruta_archivo}')
